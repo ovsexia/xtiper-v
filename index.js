66f97aac614f84d089a47142572053c91fd076e1
ovsexia <ovsexia@qq.com>
2023-02-25 10:02:42 +0800

    upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import xmsg from './lib/xmsg.js'
+import xwin from './lib/xwin.js'
+import xalert from './lib/xalert.js'
+import xconfirm from './lib/xconfirm.js'
+
+export {
+  xmsg,
+  xwin,
+  xalert,
+  xconfirm,
+}
